973023a30fcd6aef3f98b54550a4fdc365f61e3f
Ahmet Basli <ahmetbasli@ogoodigital.com>
2021-12-08 13:27:13 +0300

    Add tesla style folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 const {series} = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
 
 function buildStyles() {
   return gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'))
 }
 
